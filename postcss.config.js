e022a90975482a9d024b04b8d19aba885fb05989
DenisKhodasevich <megahitsale@gmail.com>
2020-12-16 00:56:15 +0500

    the frame of the project is made

---
+++ postcss.config.js
@@ -0,0 +1,17 @@
+const autoprefixer = require('autoprefixer');
+const cssMqpacker = require('css-mqpacker');
+
+const cssnano = require('cssnano')({
+  preset: [
+    'default',
+    { discardComments: { removeAll: true } },
+  ],
+});
+
+module.exports = {
+  plugins: [
+    autoprefixer,
+    cssMqpacker,
+    cssnano,
+  ],
+};
---
+++ webpack.base.config.js
@@ -0,0 +1,106 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
+const webpack = require('webpack');
+
+const PATHS = {
+  src: path.join(__dirname, 'src'),
+  dist: path.join(__dirname, 'dist'),
+};
+
+module.exports = {
+  entry: { app: PATHS.src },
+
+  output: {
+    path: PATHS.dist,
+    filename: '[name].js',
+  },
+
+  module: {
+    rules: [{
+      test: /\.js$/,
+      loader: 'babel-loader',
+      exclude: '/node_modules/',
+    },
+
+    {
+      test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+      loader: 'file-loader',
+      options: { name: 'fonts/[name].[ext]' },
+    },
+
+    {
+      test: /\.pug$/,
+      loader: 'pug-loader',
+      options: { pretty: true },
+    },
+
+    {
+      test: /\.scss$/,
+
+      use: [
+        'style-loader',
+        MiniCssExtractPlugin.loader,
+
+        {
+          loader: 'css-loader',
+          options: { sourceMap: true },
+        },
+
+        {
+          loader: 'postcss-loader',
+          options: { sourceMap: true },
+        },
+
+        {
+          loader: 'sass-loader',
+          options: { sourceMap: true },
+        },
+      ],
+    },
+
+    {
+      test: /\.css$/,
+
+      use: [
+        'style-loader',
+        MiniCssExtractPlugin.loader,
+
+        {
+          loader: 'css-loader',
+          options: { sourceMap: true },
+        },
+
+        {
+          loader: 'postcss-loader',
+          options: { sourceMap: true },
+        },
+      ],
+    }],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: `${PATHS.src}/index.pug`,
+    }),
+
+    new MiniCssExtractPlugin({ filename: '[name].css' }),
+
+    new CopyWebpackPlugin([{
+      from: `${PATHS.src}/+(blocks|images)/**/*.+(jpg|png|svg)`,
+      to: `${PATHS.dist}/images`,
+      flatten: true,
+    }]),
+
+    new ImageminPlugin({ test: /\.(jpe?g|png|gif|svg)$/i }),
+
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+    }),
+  ],
+};
