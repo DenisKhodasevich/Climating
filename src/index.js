561036a443390f05f104dad9c4453cf1e1f5d01f
DenisKhodasevich <megahitsale@gmail.com>
2020-12-19 18:01:58 +0500

    "Header" block made

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
+import { sidenav } from 'materialize-css';
+
 function requireAll(requireContext) {
   return requireContext.keys().map(requireContext);
 }
 
 requireAll(require.context('./', true, /\.js|scss$/));
+
+$(() => {
+  $('.sidenav').sidenav();
+});
